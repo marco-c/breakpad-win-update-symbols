35baea3a8b75222416db094c58ca936d8678a6d2
Marco Castelluccio <mcastelluccio@mozilla.com>
2019-02-12 16:50:20 +0100

    Fix tooltool URL

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ cd /home/user/dump_syms
 if test -n "$LOCAL"; then
     EXTRA="--authentication-file=/home/user/luser/tooltool-download-token"
 else
-    EXTRA="--url=http://taskcluster/mozilla-releng.net/tooltool/"
+    EXTRA="--url=http://taskcluster/tooltool.mozilla-releng.net/"
 fi
 python ../tooltool.py -v ${EXTRA} -m dump-syms.manifest fetch
 /opt/wine-staging/bin/regsvr32 msdia120.dll
